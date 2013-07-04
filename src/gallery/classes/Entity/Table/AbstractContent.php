87caacdb11c92e6916efe695150e53f08db1a893
Александр Гаврилюк <olex.goooogle@gmail.com>
2013-07-04 22:43:11 +0400

    Включено отображение неактивных изображений в АИ - http://bugs.eresus.ru/view.php?id=915 (cherry picked from commit adb48d7)

--- src/gallery/classes/Entity/Table/AbstractContent.php
+++ src/gallery/classes/Entity/Table/AbstractContent.php
@@ -51,7 +51,7 @@ abstract class Gallery_Entity_Table_AbstractContent extends ORM_Table
 	 *
 	 * @return ORM_Entity[]
 	 */
-	public function findInSection($id, $limit = null, $offset = 0, $activeOnly = true)
+	public function findInSection($id, $limit = null, $offset = 0, $activeOnly = false)
 	{
 		$q = $this->createSelectQuery();
 		$where = array($q->expr->eq('section', $q->bindValue($id, null, PDO::PARAM_INT)));
